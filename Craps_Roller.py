5a1e7be3ddfa137b8bf2b04d76d234c1b9c39b42
jnguyen <jnguyen@stcharlessd.org>
2014-09-26 04:11:47 -0500

    changed Craps_Roller.py to properly work.

--- Craps_Roller.py
+++ Craps_Roller.py
@@ -4,13 +4,13 @@
 # Last Edited: Thorin Schmidt
 # Date Edited: 9/26/2014
 
-impart random
+import random
 
 # generate random numbers 1 - 6
 die1 = random.randint(1, 6) 
 die2 = random.randrange(6)
 
-total = 'die1' + 'die2'
+total = die1 + die2
 
 print("You rolled a", die1, "and a", die2, "for a total of", total)
 
